11117da39bff3a3029734c208b042e2842880b3b
sowmik-sec <sowmik.sec@gmail.com>
2024-07-09 18:05:58 +0600

    React Project Folder Structure And Router Setup

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <h2 className="text-3xl">this is home</h2>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/routes/routes.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import "./index.css";
+import Root from "../layout/Root";
+import Home from "../pages/Home/Home";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default routes;
